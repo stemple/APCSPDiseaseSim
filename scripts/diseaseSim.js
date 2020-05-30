6e40cfd3643db478b7020328dd9ecee4ccb81f34
Steve Temple <stemple@SRHSD-11602.local>
2020-05-30 09:19:43 -0500

    Simplified for class

--- scripts/diseaseSim.js
+++ scripts/diseaseSim.js
@@ -12,18 +12,30 @@ class Person {
       this.sick = false;
       this.elem = _elem;
     }
+
+    move() {
+      this.xpos = this.xpos + this.xstep;
+      this.ypos = this.ypos + this.ystep;
+      this.elem.css("top", this.ypos);
+      this.elem.css("left", this.xpos);
+    }
   } 
   
   $(document).ready(() => {
     
-    let $container = $("#container")
-    
-    let people = [];
+    let $container = $("#container");
+    let $total = $("#total");
+    let $immune = $("#immune");
+
+    // Some paraemters for the simulation
+    let infectionDuration = 60;
     let numPeople = 300;
-    let numInfected = 0;
+    
+    // Counter for tracking the people who have healed
     let numHealed = 0;
 
     // Create the population of people
+    let people = [];
     for(let i = 0; i < numPeople; i++) {
       var div = document.createElement("div");
       div.id = i;
@@ -34,8 +46,8 @@ class Person {
         i,
         Math.random() * 790, 
         Math.random() * 390,
-        Math.random() * 5 - 2.5,
-        Math.random() * 5 - 2.5, 
+        Math.random() * 4 - 2,
+        Math.random() * 4 - 2, 
         $elem));
     }
     
@@ -45,13 +57,59 @@ class Person {
     people[0].xstep = 2;
     people[0].ystep = 1;
     people[0].infected = true;
-    people[0].infectious = 100;
+    people[0].infectious = infectionDuration;
     people[0].elem.addClass("infected");
+    
+    let id = setInterval(step, 50);
+    
+    // The main program loop - animate all the people moving
+    function step() {
+     
+      for(let i = 0; i < numPeople; i++) {
+        var p = people[i];
+       
+        // decrement infectious timer
+        if(p.infectious > 0) {
+          p.infectious = p.infectious - 1;
+        }
+        
+        // Have they developed antibodies?
+        if(p.infected == true && p.antibodies == false && p.infectious <= 0) {
+          numHealed = numHealed + 1;
+          p.antibodies = true;
+          p.infected = false;
+          p.xstep = Math.random() * 5 - 2.5;
+          p.ystep = Math.random() * 5 - 2.5;
+          p.elem.addClass("antibodies");
+          p.elem.removeClass("infected");
+        }
 
-    numInfected = 1;
-    
-    let id = setInterval(frame, 50);
-    
+        // Check if person should "bounce" off container wall
+        if (p.xpos > 790 || p.xpos < 0) {
+          p.xstep = -p.xstep;
+        }
+        if (p.ypos > 390 || p.ypos < 0) {
+          p.ystep = -p.ystep;
+        }
+        
+        // Check for infection, but only for not infected people
+        if(p.antibodies != true) {
+          if(p.infected != true) {
+            checkForInfection(p);
+          }
+        }
+
+        // Move the person.
+        p.move();
+        
+      }
+
+      // Set the labels for tracking the sim numbers
+      $total.text(`Total: ${numPeople}`);
+      $immune.text(`Healed: ${numHealed}`);
+
+    }
+
     // Collision algorithm to check if a person has become infected
     function checkForInfection(p) {
       for(let i = 0; i < numPeople; i++) {
@@ -64,83 +122,14 @@ class Person {
           
           if (distance < p.radius + people[i].radius) {
             // collision detected! Infection ocurrs
-            // console.log(p.id, "Infected!")
-            // 50% chance of infection
-            let chance =  Math.round(Math.random());
-            if(chance == 1) {
-              numInfected = numInfected + 1;
-              p.infected = true;
-              // timer for how long a person is infectious
-              p.infectious = 200;
-              p.elem.addClass("infected");
-            
-              // 50% people become sick and immobile
-              // 50% are nonsymptomatic carriers
-              p.sick = Math.round(Math.random());
-              if(p.sick == 1) {
-                p.xstep = 0;
-                p.ystep = 0;
-              }
-            }
+            p.infected = true;
+            // timer for how long a person is infectious
+            p.infectious = infectionDuration;
+            p.elem.addClass("infected");
           }
        
          }
        }
-    }
-    
-    // The main program loop - animate all the people moving
-    function frame() {
-     
-      for(let i = 0; i < numPeople; i++) {
-        var p = people[i];
-       
-        // decrement infectious timer
-        if(p.infectious > 0) {
-          p.infectious = p.infectious - 1;
-        }
-        
-        // Have they developed antibodies?
-        if(p.infected == true && p.antibodies == false && p.infectious <= 0) {
-          //console.log(p.id, "Healed!")
-          numInfected = numInfected - 1;
-          numHealed = numHealed + 1;
-          p.antibodies = true;
-          p.infected = false;
-          p.sick = false;
-          p.xstep = Math.random() * 5 - 2.5;
-          p.ystep = Math.random() * 5 - 2.5;
-          p.elem.addClass("antibodies");
-          p.elem.removeClass("infected");
-        }
-
-        // Move the person
-        if (p.xpos > 790 || p.xpos < 0) {
-          p.xstep = -p.xstep;
-        }
-        if (p.ypos > 390 || p.ypos < 0) {
-          p.ystep = -p.ystep;
-        }
-  
-        p.xpos = p.xpos + p.xstep;
-        p.ypos = p.ypos + p.ystep;
-
-        // update position on screen
-        p.elem.css("top", p.ypos);
-        p.elem.css("left", p.xpos);
-        
-        // Check for infection
-        if(p.antibodies != true) {
-          if(p.infected != true) {
-            checkForInfection(p);
-          }
-        }
-        
-      }
-
-      console.log("Infected", numInfected);
-      console.log("Not Infected", numPeople - numInfected);
-      console.log("Healed", numHealed);
-
     }
     
   });
